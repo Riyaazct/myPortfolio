2a42669334055cdd015350c80dd9b92c9f38e795
Riyaazct <riyaazct1@gmail.com>
2023-09-03 12:53:28 +0200

    adjust: styling for projects section cards

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,7 +11,6 @@ const Projects = () => {
           Projects
         </h2>
       </div>
-
       {/* <!-- Inside the Projects section of portfolio --> */}
       {/* <div className="flex justify-center mt-6">
           <button
@@ -27,12 +26,11 @@ const Projects = () => {
             Full-Stack Projects
           </button>
         </div> */}
-
       {/* Project Container */}
-      <div className="grid grid-cols-1 gap-5 p-5 mx-auto lg:grid-cols-2 lg:max-w-[1300px]">
+      <div className="grid grid-cols-1 gap-4 md:p-5 mx-auto lg:grid-cols-2 lg:max-w-[1300px]">
         {/* Project Card 1 */}
-        <div className="flex flex-col items-center p-1 m-2 border-2 rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
-          <h3 className="py-4 text-2xl font-semibold text-[#1E1E1E] ">
+        <div className="w-full flex flex-col items-center p-1 md:border-2 md:rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
+          <h3 className="py-4 text-2xl font-semibold text-[#FFFFFF]">
             TV Show Explorer
           </h3>
           <img
@@ -43,7 +41,7 @@ const Projects = () => {
           <p className="px-6 sm:px-9 py-2 text-white text-start md:max-w-[740px]">
             During my bootcamp, I completed a project focused on
             manipulating the DOM and fetching data from the TVmaze API
-            using {/* */}
+            using{" "}
             <span className="text-[#FFD700] font-mediums">
               HTML, CSS and JavaScript
             </span>
@@ -85,8 +83,8 @@ const Projects = () => {
         {/*end of project card 1 */}
 
         {/* Project Card 2 */}
-        <div className="flex flex-col items-center p-1 m-2 border-2 rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
-          <h3 className="py-4 text-2xl font-semibold text-[#1E1E1E] ">
+        <div className="w-full flex flex-col items-center p-1 md:border-2 md:rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
+          <h3 className="py-4 text-2xl font-semibold text-[#FFFFFF]">
             TV Show Explorer
           </h3>
           <img
@@ -97,66 +95,7 @@ const Projects = () => {
           <p className="px-6 sm:px-9 py-2 text-white text-start md:max-w-[740px]">
             During my bootcamp, I completed a project focused on
             manipulating the DOM and fetching data from the TVmaze API
-            using {/* */}
-            <span className="text-[#FFD700] font-mediums">
-              HTML, CSS and JavaScript
-            </span>
-            . The TV Show Explorer allowed users to search and explore
-            TV shows, displaying details such as titles, genres,
-            ratings, and runtime in a dynamic and user-friendly
-            interface. This project showcased my ability to work with
-            external APIs and create engaging web applications using
-            fundamental web development technologies.
-          </p>
-          <ul className="flex gap-5 text-[#ffffff] font-semibold text-sm mt-4 border bg-slate-500 p-2 rounded-md">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-          </ul>
-          <div className="flex gap-6 mb-5 text-white">
-            <a
-              href="https://cyf-riyaazct-tv.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex flex-col items-center mt-3">
-                <SlLink size={22} color="#FFD700" />
-                <p className="pt-2 text-sm">Live Link</p>
-              </div>
-            </a>
-            <a
-              href="https://github.com/Riyaazct/tv-show-dom-project"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex flex-col items-center mt-3">
-                <SiGithub size={22} color="#FFD700" />
-                <p className="pt-2 text-sm">View code</p>
-              </div>
-            </a>
-          </div>
-
-          {/* End of Project Card 2 */}
-        </div>
-
-        {/*  */}
-
-        {/*  */}
-
-        {/* Project Card 1 */}
-        <div className="flex flex-col items-center p-1 m-2 border-2 rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
-          <h3 className="py-4 text-2xl font-semibold text-[#1E1E1E] ">
-            TV Show Explorer
-          </h3>
-          <img
-            src={tvShowDomImg}
-            alt="tv shows project"
-            className="max-w-2xl w-[90%] my-7"
-          />
-          <p className="px-6 sm:px-9 py-2 text-white text-start md:max-w-[740px]">
-            During my bootcamp, I completed a project focused on
-            manipulating the DOM and fetching data from the TVmaze API
-            using {/* */}
+            using{" "}
             <span className="text-[#FFD700] font-mediums">
               HTML, CSS and JavaScript
             </span>
@@ -195,11 +134,11 @@ const Projects = () => {
             </a>
           </div>
         </div>
-        {/*end of project card 1 */}
+        {/*end of project card 2 */}
 
-        {/* Project Card 2 */}
-        <div className="flex flex-col items-center p-1 m-2 border-2 rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
-          <h3 className="py-4 text-2xl font-semibold text-[#1E1E1E] ">
+        {/* Project Card 3 */}
+        <div className="w-full flex flex-col items-center p-1 md:border-2 md:rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
+          <h3 className="py-4 text-2xl font-semibold text-[#FFFFFF]">
             TV Show Explorer
           </h3>
           <img
@@ -210,7 +149,7 @@ const Projects = () => {
           <p className="px-6 sm:px-9 py-2 text-white text-start md:max-w-[740px]">
             During my bootcamp, I completed a project focused on
             manipulating the DOM and fetching data from the TVmaze API
-            using {/* */}
+            using{" "}
             <span className="text-[#FFD700] font-mediums">
               HTML, CSS and JavaScript
             </span>
@@ -248,12 +187,64 @@ const Projects = () => {
               </div>
             </a>
           </div>
-
-          {/* End of Project Card 2 */}
         </div>
+        {/*end of project card 3 */}
 
-        {/* end of Project Container */}
-      </div>
+        {/* Project Card 4 */}
+        <div className="w-full flex flex-col items-center p-1 md:border-2 md:rounded-md border-gray-950 bg-[#2E3B4E] shadow-sm shadow-[#1E1E1E] lg:hover:shadow-md lg:hover:scale-[1.01] lg:hover:duration-700">
+          <h3 className="py-4 text-2xl font-semibold text-[#FFFFFF]">
+            TV Show Explorer
+          </h3>
+          <img
+            src={tvShowDomImg}
+            alt="tv shows project"
+            className="max-w-2xl w-[90%] my-7"
+          />
+          <p className="px-6 sm:px-9 py-2 text-white text-start md:max-w-[740px]">
+            During my bootcamp, I completed a project focused on
+            manipulating the DOM and fetching data from the TVmaze API
+            using{" "}
+            <span className="text-[#FFD700] font-mediums">
+              HTML, CSS and JavaScript
+            </span>
+            . The TV Show Explorer allowed users to search and explore
+            TV shows, displaying details such as titles, genres,
+            ratings, and runtime in a dynamic and user-friendly
+            interface. This project showcased my ability to work with
+            external APIs and create engaging web applications using
+            fundamental web development technologies.
+          </p>
+          <ul className="flex gap-5 text-[#ffffff] font-semibold text-sm mt-4 border bg-slate-500 p-2 rounded-md">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+          </ul>
+          <div className="flex gap-6 mb-5 text-white">
+            <a
+              href="https://cyf-riyaazct-tv.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="flex flex-col items-center mt-3">
+                <SlLink size={22} color="#FFD700" />
+                <p className="pt-2 text-sm">Live Link</p>
+              </div>
+            </a>
+            <a
+              href="https://github.com/Riyaazct/tv-show-dom-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="flex flex-col items-center mt-3">
+                <SiGithub size={22} color="#FFD700" />
+                <p className="pt-2 text-sm">View code</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        {/*end of project card 4 */}
+      </div>{" "}
+      {/* end of Project Container */}
     </section>
   );
 };
