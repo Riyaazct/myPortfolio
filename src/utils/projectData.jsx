5299115fefcf6f38599f36f728f6cddc6db30e37
Riyaazct <riyaazct1@gmail.com>
2023-09-12 02:01:13 +0200

    add projects to project section

--- src/utils/projectData.jsx
+++ src/utils/projectData.jsx
@@ -1,7 +1,85 @@
 import project01 from "../assets/images/project01.png";
+import project02 from "../assets/images/project02.png";
 
 const projectData = [
   {
+    title: "Edufocus Projects",
+    image: project01,
+    description: (
+      <>
+        "Our final project for an NGO partner was a collaborative
+        effort involving a team of 7, including 4 full-stack
+        developers (students), a front-end and backend tech lead, and
+        a scrum master (volunteers). We diligently followed agile
+        methodologies to ensure project success. I took responsibility
+        for hosting the website on my GitHub repository. This role
+        also involved efficiently managing merge conflicts,
+        particularly during the initial learning phase of GitHub
+        collaboration. I played a vital role in developing several key
+        pages, including the 'About Us' page and all its' contents and
+        the 'Our Photos' page, where I meticulously organized and
+        stored data for efficient image loading from the database.
+        Additionally, I contributed to the creation of the admin page,
+        enabling the addition and management of training materials
+        within the database. The technologies employed in this project
+        encompassed{" "}
+        <span className="text-[#FFD700] font-medium">
+          {" "}
+          JavaScript, React, Node.js, Express, PostgreSQL, and Vanilla
+          CSS
+        </span>
+        . Our collective effort resulted in the successful completion
+        of the project, demonstrating our ability to work cohesively
+        and deliver a valuable solution to our NGO partner."
+      </>
+    ),
+    technologies: [
+      "Full Stack",
+      "JavaScript",
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "PostgreSQL",
+      "Github",
+      "Agile",
+      "",
+    ],
+    liveLink: "https://starter-kit-g0y0.onrender.com/",
+    githubLink: "https://github.com/Riyaazct/no-bugs-edufocus-za",
+  },
+  {
+    title: "Exploring SA",
+    image: project02,
+    description: (
+      <>
+        After graduating from a bootcamp, I embarked on a project
+        using{" "}
+        <span className="text-[#FFD700] font-medium">
+          JavaScript, React, and Tailwind CSS
+        </span>
+        . This{" "}
+        <span className="text-[#FFD700] font-medium">front-end</span>{" "}
+        website, following a mobile-first approach, optimizes client
+        hosting costs. Future plans include adding a backend. I
+        integrated a contact form via EmailJS and a WhatsApp widget
+        for seamless communication. Implementing SEO in{" "}
+        <span className="text-[#FFD700] font-medium">React</span>, I
+        strategically used keywords with meta tags. Overcoming hosting
+        and DNS challenges, I ensured continuous email functionality.
+      </>
+    ),
+    technologies: [
+      "Front End",
+      "React.js",
+      "JavaScript",
+      "Tailwindcss",
+      "Emailjs",
+      "SEO",
+    ],
+    liveLink: "https://exploringsa.co.za/",
+    githubLink: "https://github.com/Riyaazct/exploringsa",
+  },
+  {
     title: "TV Show Explorer",
     image: project01,
     description: (
@@ -9,7 +87,7 @@ const projectData = [
         During my bootcamp, I completed a project focused on
         manipulating the DOM and fetching data from the TVmaze API
         using{" "}
-        <span className="text-[#FFD700] font-mediums">
+        <span className="text-[#FFD700] font-medium">
           HTML, CSS and JavaScript
         </span>
         . The TV Show Explorer allowed users to search and explore TV
@@ -32,53 +110,7 @@ const projectData = [
         During my bootcamp, I completed a project focused on
         manipulating the DOM and fetching data from the TVmaze API
         using{" "}
-        <span className="text-[#FFD700] font-mediums">
-          HTML, CSS and JavaScript
-        </span>
-        . The TV Show Explorer allowed users to search and explore TV
-        shows, displaying details such as titles, genres, ratings, and
-        runtime in a dynamic and user-friendly interface. This project
-        showcased my ability to work with external APIs and create
-        engaging web applications using fundamental web development
-        technologies.
-      </>
-    ),
-    technologies: ["HTML", "CSS", "JavaScript"],
-    liveLink: "https://cyf-riyaazct-tv.netlify.app/",
-    githubLink: "https://github.com/Riyaazct/tv-show-dom-project/",
-  },
-  {
-    title: "TV Show Explorer",
-    image: project01,
-    description: (
-      <>
-        During my bootcamp, I completed a project focused on
-        manipulating the DOM and fetching data from the TVmaze API
-        using{" "}
-        <span className="text-[#FFD700] font-mediums">
-          HTML, CSS and JavaScript
-        </span>
-        . The TV Show Explorer allowed users to search and explore TV
-        shows, displaying details such as titles, genres, ratings, and
-        runtime in a dynamic and user-friendly interface. This project
-        showcased my ability to work with external APIs and create
-        engaging web applications using fundamental web development
-        technologies.
-      </>
-    ),
-    technologies: ["HTML", "CSS", "JavaScript"],
-    liveLink: "https://cyf-riyaazct-tv.netlify.app/",
-    githubLink: "https://github.com/Riyaazct/tv-show-dom-project/",
-  },
-  {
-    title: "TV Show Explorer",
-    image: project01,
-    description: (
-      <>
-        During my bootcamp, I completed a project focused on
-        manipulating the DOM and fetching data from the TVmaze API
-        using{" "}
-        <span className="text-[#FFD700] font-mediums">
+        <span className="text-[#FFD700] font-medium">
           HTML, CSS and JavaScript
         </span>
         . The TV Show Explorer allowed users to search and explore TV
